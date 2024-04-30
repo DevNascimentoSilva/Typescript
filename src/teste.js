d6b80877d65b0c1b313f8b53ad6ec6536c5eaf3b
Diego Silva <nascimentod322@gmail.com>
2024-04-30 10:37:59 -0300

    first commit

---
+++ src/teste.js
@@ -0,0 +1,5 @@
+"use strict";
+function multiplicar() {
+    const multiplcacao = 3 * 5;
+    console.log(multiplicar);
+}
---
+++ src/teste.ts
@@ -0,0 +1,11 @@
+function multiplicar() {
+    const multiplcacao = 3 * 5
+    console.log(multiplicar);
+}
+
+function retornaNome() {
+    const nome = 'Diego';
+    console.log('Ola + ', nome)
+
+}
+
